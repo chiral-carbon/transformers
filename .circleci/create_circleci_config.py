69fd93e4a812b967cce3eb91032005f36672aa68
ydshieh <ydshieh@users.noreply.github.com>
2023-08-05 13:04:47 +0200

    fix

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -304,7 +304,8 @@ torch_jobx = CircleCIJob(
         "pip install -U --upgrade-strategy eager .[sklearn,torch,testing,sentencepiece,torch-speech,vision,timm]",
         "pip install -U --upgrade-strategy eager git+https://github.com/huggingface/accelerate",
     ],
-    parallelism=3,
+    parallelism=1,
+    pytest_num_workers=3,
 )
 
 
