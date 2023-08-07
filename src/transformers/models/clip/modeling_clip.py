649b78a70ce1acd11ecadb600abf63ef5f70570e
ydshieh <ydshieh@users.noreply.github.com>
2023-08-07 21:56:25 +0200

    [skip ci] fix

--- src/transformers/models/clip/modeling_clip.py
+++ src/transformers/models/clip/modeling_clip.py
@@ -1198,7 +1198,7 @@ class CLIPModel(CLIPPreTrainedModel):
 class CLIPTextModelWithProjection(CLIPPreTrainedModel):
     config_class = CLIPTextConfig
 
-    _no_split_modules = ["CLIPEncoderLayer"]
+    _no_split_modules = ["CLIPTextEmbeddings", "CLIPEncoderLayer"]
 
     def __init__(self, config: CLIPTextConfig):
         super().__init__(config)
